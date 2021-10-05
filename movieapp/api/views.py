39ae155bbe755a567a4bd0f573ee463d90205152
Said ZITOUNI <zitouni.sd@gmail.com>
2021-10-05 11:28:59 +0100

    resolve bug related to foreign key , no need to use id django we can pass the instance and django figure out the rest

--- movieapp/api/views.py
+++ movieapp/api/views.py
@@ -1,44 +1,44 @@
 from django.contrib.auth.models import User
 from django.shortcuts import render
-from rest_framework import viewsets,status
+from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from .serializers import MovieSerializer,RatingSerializer
-from .models import Movie,Rating
+from .serializers import MovieSerializer, RatingSerializer
+from .models import Movie, Rating
+
 
 # Create your views here. example of view as function
 def test(request):
-    return render(request,template_name='test.html')
+    return render(request, template_name='test.html')
 
-#Class viewsets
+
+# Class viewsets
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
 
-    @action(detail = True,methods = ['POST','GET'])
-    def rate_movie(self,request,pk=None):
+    @action(detail=True, methods=['POST', 'GET'])
+    def rate_movie(self, request, pk=None):
         if 'stars' in request.data:
             movie = Movie.objects.get(id=pk)
             stars = request.data['stars']
-            #user = request.user
+            # user = request.user
             user = User.objects.get(id=1)
-            print('user',user.username)
+            print('user', user.username)
+            print(movie.title)
 
             try:
-                rating = Rating.objects.get(user=user.id,movie=movie.id)
+                rating = Rating.objects.get(user=user, movie=movie)
                 rating.stars = stars
                 rating.save()
-                serializer = RatingSerializer(rating,many=False)
+                serializer = RatingSerializer(rating, many=False)
                 response = {'message': 'Rating updated', 'result ': serializer.data}
                 return Response(response, status=status.HTTP_200_OK)
             except:
-                rating = Rating.objects.create(user=user.id, movie=movie.id,stars=stars)
-                serializer = RatingSerializer(rating,many=False)
+                rating = Rating.objects.create(user=user, movie=movie, stars=stars)
+                serializer = RatingSerializer(rating, many=False)
                 response = {'message': 'Rating created', 'result ': serializer.data}
                 return Response(response, status=status.HTTP_200_OK)
-
-
-
 
 
 class RatingViewSet(viewsets.ModelViewSet):
